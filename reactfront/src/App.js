059c7bb152536f3245da61604c7d926f372f128b
Luis Angel Mendoza Chate <49343533+lmendoza27@users.noreply.github.com>
2022-03-06 12:10:18 -0500

    Nuevo

---
+++ reactfront/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import ShowProducts from './components/ShowProducts';
+import CreateProducts from './components/CreateProducts';
+import EditProducts from './components/EditProducts';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <ShowProducts/> }/>
+          <Route path='/create' element={ <CreateProducts/> }/>
+          <Route path='/edit/:id' element={ <EditProducts/> }/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactfront/src/components/ShowProducts.js
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
+const endpoint = 'http://127.0.0.1:8000/api'
+
+const ShowProducts = () => {
+
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        getAllProducts()
+    }, [])
+
+    const getAllProducts = async () => {
+        const response = await axios.get(`${endpoint}/products`)
+        setProducts(response.data)
+    }
+
+    const deleteProduct = async (id) => {
+        await axios.delete(`${endpoint}/product/${id}`)
+        getAllProducts()
+    }
+
+  return (
+    <div>
+        <div className="d-grip gap-2">
+            <Link to="/create" className="btn btn-success btn-lg mt-2 mb-2 text-white">Create</Link>
+        </div>
+
+        <table className="table table-striped">
+            <thead className="bg-primary text-white">
+                <tr>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Stock</th>  
+                <th>Actions</th>  
+                </tr>    
+            </thead>
+        <tbody>
+        { products.map( (product)=>(
+            <tr key={product.id}>
+                <td>{product.description}</td>
+                <td>{product.price}</td>
+                <td>{product.stock}</td>
+                <td>
+                    <Link to={`/edit/${product.id}`} className="btn btn-warning">Edit</Link>
+                    <button onClick={ ()=>deleteProduct(product.id) } className="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+        ) ) }
+        </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ShowProducts
