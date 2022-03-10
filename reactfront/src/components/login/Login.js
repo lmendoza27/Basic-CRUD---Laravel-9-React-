6a3362101a03f83d34186fa075ebb8072f8882fe
Luis Angel Mendoza Chate <49343533+lmendoza27@users.noreply.github.com>
2022-03-10 13:17:59 -0500

    Changes

--- reactfront/src/components/login/Login.js
+++ reactfront/src/components/login/Login.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
-//import axios from 'axios'
+import React, { Component, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2';
 
 class Login extends Component {
+
     constructor()
     {
         super();
@@ -9,7 +11,12 @@ class Login extends Component {
             email:null,
             password:null,
             login:false,
-            store:null
+            store:null,
+            description:null,
+            price:null,
+            stock:null,
+            items: [],
+            DataisLoaded: false
         }
 
     }
@@ -17,7 +24,27 @@ class Login extends Component {
     componentDidMount()
     {
         this.storeCollector()
+
+        // Fetch Data
+        this.getProducts()
+            
     }
+
+    getProducts()
+    {
+
+        fetch(
+            "http://127.0.0.1:8000/api/products")
+                        .then((res) => res.json())
+                        .then((json) => {
+                            this.setState({
+                                items: json,
+                                DataisLoaded: true
+                            });
+                        })
+    }
+
+
     storeCollector()
     {
         let store = JSON.parse(localStorage.getItem('login'));
@@ -51,6 +78,9 @@ class Login extends Component {
 
     post() 
     {
+        this.inputDescription.value = "";
+        this.inputPrice.value = "";
+        this.inputStock.value = "";
         //alert(this.state.store.access_token)
         //alert(localStorage.getItem('login'))
         let access_token="Bearer "+ this.state.store.store
@@ -63,23 +93,40 @@ class Login extends Component {
                 //"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC92MVwvYXV0aFwvbG9naW4iLCJpYXQiOjE2NDY4NzAwNDUsImV4cCI6MTY0Njg3MzY0NSwibmJmIjoxNjQ2ODcwMDQ1LCJqdGkiOiJLN1l3d2wxMlE3ekZlalB1Iiwic3ViIjoxLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.hIrJROQFhIycxR4UiziUrSae5TJCBDy-E_ewqcxCcMw"
               },
             //credentials: "same-origin",
-            body:JSON.stringify(this.posted)
+            body:JSON.stringify(this.state)
         }).then((response)=>{
             response.json().then((result)=>{
                 console.warn("result",result);
- 
             })
+            //alert('Producto Registrado :)')
+            Swal.fire({
+                icon:"success",
+                text:this.state.description + " Agregado"
+              })
+            this.getProducts()
         })
     }
 
+    logout()
+    {
+        alert("Suerte!!!")
+        //this.navigate('/')
+    }
+
     render() {
+
+        const { DataisLoaded, items } = this.state;
+
+        if (!DataisLoaded) return <div>
+        <h1> Espere un momento.... </h1> </div> ;
+
         return (
             <div>
             <h1>JWT Token with React</h1>
             {
                 !this.state.login?
                 <div>
-                <input type="text" onChange={(event)=>{this.setState({email:event.target.value})}} /> <br></br>
+                <input type="text"  onChange={(event)=>{this.setState({email:event.target.value})}} /> <br></br>
                 <input type="password" onChange={(event)=>{this.setState({password:event.target.value})}} /> <br></br>
     
                 <button onClick={()=>{this.login()}} >Login</button>
@@ -87,27 +134,34 @@ class Login extends Component {
                 :
                 <div>
                     <h2>Bienvenido</h2>
-
-                    {/* Así se comenta en JSX */}
-                    {/* <textarea onChange={(event)=>{this.setState({post:event.target.value})}}>
-                    
-            </textarea> */}
-                <input type="text" onChange={(event)=>{this.setState({description:event.target.value})}} /> <br></br>
-                <input type="number" onChange={(event)=>{this.setState({price:event.target.value})}} /> <br></br>
-                <input type="number" onChange={(event)=>{this.setState({stock:event.target.value})}} /> <br></br>
-
+                <label>Description</label><br/>
+                <input type="text" ref={el => this.inputDescription = el}  onChange={(event)=>{this.setState({description:event.target.value})}} /> <br></br>
+                <label>Price</label><br/>
+                <input type="number" ref={el => this.inputPrice = el} onChange={(event)=>{this.setState({price:event.target.value})}} /> <br></br>
+                <label>Stock</label><br/>
+                <input type="number" ref={el => this.inputStock = el} onChange={(event)=>{this.setState({stock:event.target.value})}} /> <br></br>
 
                     <br></br>
-                    <button onClick={()=>{this.post()}}>POST</button>
-                </div>
+                    <button type="button" class="btn btn-primary btn-lg btn-block" onClick={()=>{this.post()}}>POST</button>
+                <br></br>
+                <h3>Listado de Productos</h3>
+            {
+                items.map((item) => ( 
+                <ol key = { item.id } >
+                    Description: { item.description }, 
+                    Price: { item.price }, 
+                    Stock: { item.stock }  
+                    </ol>
+                ))
             }
+     
+        <button onClick={()=>{this.logout()}} >Logout</button>
 
+                </div>   
+            }
         </div>
         );
     }
-
-
 }
-
 
 export default Login
